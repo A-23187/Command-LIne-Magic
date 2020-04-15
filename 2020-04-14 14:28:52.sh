0fd87ef71d087e5d06792913ccf7b5470c664e93
A23187 <a23187.1979421048@gmail.com>
2020-04-15 01:02:56 +0000

    Add '2020-04-14 14:28:52.sh'

---
+++ 2020-04-14 14:28:52.sh
@@ -0,0 +1,4 @@
+# Make a maze! POSIX compliant version and 15 characters shorter than the C64 version. It may not work on the BSD version of tr. :P (Updated on 2020-04-14 14:28:52)
+
+tr -cd /\\</dev/urandom
+
