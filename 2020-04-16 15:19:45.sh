1f199f52f95a31a8545c1ebd68d632c3e16290ac
A23187 <a23187.1979421048@gmail.com>
2020-04-17 01:03:13 +0000

    Add '2020-04-16 15:19:45.sh'

---
+++ 2020-04-16 15:19:45.sh
@@ -0,0 +1,4 @@
+# Upload to youtube, but limit the rate to 1000kbps between 9 and 5pm. Get the program at http://bit.ly/2QwRfzV (Updated on 2020-04-16 15:19:45)
+
+youtubeuploader -title "Cat Mischief" -description "50 minutes of my cat knocking stuff over" -limitBetween 09:00-17:00 -ratelimit 1000 -filename cat.webm
+
