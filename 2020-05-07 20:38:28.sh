0c3890bf2d29e774a9566388ff0ffd0def07446b
A23187 <a23187.1979421048@gmail.com>
2020-05-08 01:07:05 +0000

    Add '2020-05-07 20:38:28.sh'

---
+++ 2020-05-07 20:38:28.sh
@@ -0,0 +1,4 @@
+# You can actually follow more than one log at once and get new updates on them. Use -q to not print filename header, but that might be confusing. (Updated on 2020-05-07 20:38:28)
+
+tail -F *.log
+
