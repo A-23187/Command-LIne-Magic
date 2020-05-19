57afb703079fb8ce1a79f6c5478bf0591aeb9714
A23187 <a23187.1979421048@gmail.com>
2020-05-19 01:03:52 +0000

    Add '2020-05-18 13:42:28.sh'

---
+++ 2020-05-18 13:42:28.sh
@@ -0,0 +1,4 @@
+# Run yourscript.py, then put yourself into the python interactive console. Nice for when you're debugging problems/playing around and want to front load a bunch of code. (Updated on 2020-05-18 13:42:28)
+
+python3 -i yourscript.py
+
