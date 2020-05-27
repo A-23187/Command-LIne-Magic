383763fbfaee1c2eb08ee108d304d0f63a4f18ac
A23187 <a23187.1979421048@gmail.com>
2020-05-27 13:05:37 +0000

    Add '2020-05-27 11:05:28.sh'

---
+++ 2020-05-27 11:05:28.sh
@@ -0,0 +1,4 @@
+# Another weather option if you don't want to fill your terminal and want something more terse is ansiweather by @fcambus . Maybe just make an alias for your location and desired options. [Image:20200527_070219.44_1196x677_scrot.png] (Updated on 2020-05-27 11:05:28)
+
+ansiweather -u imperial -l "Elko, NV"
+
