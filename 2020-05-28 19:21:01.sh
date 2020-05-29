4ef41ea34d552e40b6f7cfa7664464fe76b479e0
A23187 <a23187.1979421048@gmail.com>
2020-05-29 01:04:59 +0000

    Add '2020-05-28 19:21:01.sh'

---
+++ 2020-05-28 19:21:01.sh
@@ -0,0 +1,4 @@
+# Create a live updated date file for use as a text output with OBS video studio so you can have the date within your live video scene. Or whatever other command you want to show. (Updated on 2020-05-28 19:21:01)
+
+while true ; do date > date-output.txt ; sleep 1 ; done
+
