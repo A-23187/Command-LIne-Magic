98474a88bbaa2e4dc5e2986201bca7a988f477e3
A23187 <a23187.1979421048@gmail.com>
2020-06-03 01:05:19 +0000

    Add '2020-06-02 18:43:42.sh'

---
+++ 2020-06-02 18:43:42.sh
@@ -0,0 +1,4 @@
+# Where is that picture of Eric I took in the past year? I think I put her name in it. Use -iname for case insensitive filename searches. Use -mtime -365 for modified less than a year ago. (Updated on 2020-06-02 18:43:42)
+
+find ~/ -iname '*eric*.jpg' -mtime -365
+
