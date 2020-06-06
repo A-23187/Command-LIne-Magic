7af17971b4720d0504c73e6e67d4ab47a918c685
A23187 <a23187.1979421048@gmail.com>
2020-06-06 01:05:05 +0000

    Add '2020-06-05 13:40:34.sh'

---
+++ 2020-06-05 13:40:34.sh
@@ -0,0 +1,4 @@
+# Easily makes a full filesystem json output including full path, file permissions, owner, size, etc. Thx @aborruso and @saulfp (Updated on 2020-06-05 13:40:34)
+
+sudo tree -Jhfpug  /
+
