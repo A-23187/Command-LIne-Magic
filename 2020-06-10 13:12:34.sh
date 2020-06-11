bd10dee314be64b87fb5b3aefb0123400d02bf8c
A23187 <a23187.1979421048@gmail.com>
2020-06-11 01:05:27 +0000

    Add '2020-06-10 13:12:34.sh'

---
+++ 2020-06-10 13:12:34.sh
@@ -0,0 +1,4 @@
+# Make a quick rot13 translation quick reference table. You could also start at different positions on the 2nd command to control the shift amount for various cipher cracking games. (Updated on 2020-06-10 13:12:34)
+
+echo {a..z} ; echo {n..z} {a..m}
+
