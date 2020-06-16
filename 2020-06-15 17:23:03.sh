62fee04f10e3627a79037f8bdb4a9c5957fcd525
A23187 <a23187.1979421048@gmail.com>
2020-06-16 01:05:35 +0000

    Add '2020-06-15 17:23:03.sh'

---
+++ 2020-06-15 17:23:03.sh
@@ -0,0 +1,4 @@
+# Use -I to force case insensitive searches. If you use the -i option to less, it only ignores case if you do not use uppercase letters in your search pattern. With -i, any uppercase letter used in the search pattern and it changes the search to be case sensitive. (Updated on 2020-06-15 17:23:03)
+
+less -I
+
