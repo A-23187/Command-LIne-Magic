fd62659d7d669001600211a2a4c13aaaa1ab918f
A23187 <a23187.1979421048@gmail.com>
2020-07-10 01:06:34 +0000

    Add '2020-07-09 13:56:00.sh'

---
+++ 2020-07-09 13:56:00.sh
@@ -0,0 +1,4 @@
+# script is a nice program that allows you to record your interactive shell session and records everything that seen in your terminal. Just exit the shell when done. Great for logging upgrades, audits, and general CYA work. (Updated on 2020-07-09 13:56:00)
+
+script upgrade.log
+
