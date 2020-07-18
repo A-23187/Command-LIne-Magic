311025e505f694e47c9846bc3d42b560cdb9ffb1
A23187 <a23187.1979421048@gmail.com>
2020-07-18 01:07:20 +0000

    Add '2020-07-17 18:38:06.sh'

---
+++ 2020-07-17 18:38:06.sh
@@ -0,0 +1,4 @@
+# What country does your email flow through? Is it was you expected? Change demon.net in this command to the domain you use. (Updated on 2020-07-17 18:38:06)
+
+geoiplookup $( dig +short MX demon.net )
+
