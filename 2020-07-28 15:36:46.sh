5adbd89632c3ef3cb3490b842080c4fa7f25def0
A23187 <a23187.1979421048@gmail.com>
2020-07-29 01:07:41 +0000

    Add '2020-07-28 15:36:46.sh'

---
+++ 2020-07-28 15:36:46.sh
@@ -0,0 +1,4 @@
+# Unless you've had some filesystem trouble or migrated to a new root filesystem, this will usually indicate when you installed a system. (Updated on 2020-07-28 15:36:46)
+
+ls -ld /lost+found
+
