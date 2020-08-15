215d31fff8d03028cbc2f9a28b7bf713179427c2
A23187 <a23187.1979421048@gmail.com>
2020-08-15 01:08:40 +0000

    Add '2020-08-14 14:54:55.sh'

---
+++ 2020-08-14 14:54:55.sh
@@ -0,0 +1,4 @@
+# Use a CIDR network format aware version of grep to search for hits from IPs within the IPv6 range 2102:306:146e:f5d0::/64 (Updated on 2020-08-14 14:54:55)
+
+grepcidr 2102:306:146e:f5d0::/64 /var/log/secure
+
