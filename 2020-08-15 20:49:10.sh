91f07bbaaafc23e37607826dc8cbeaabe40af87d
A23187 <a23187.1979421048@gmail.com>
2020-08-16 01:08:20 +0000

    Add '2020-08-15 20:49:10.sh'

---
+++ 2020-08-15 20:49:10.sh
@@ -0,0 +1,4 @@
+# Try weechat for an CLI IRC client. There is also an official IRC channel for climagic on irc.freenode.org. It's a great place for discussing command line tricks in general. (Updated on 2020-08-15 20:49:10)
+
+weechat irc://irc.freenode.org/#climagic
+
