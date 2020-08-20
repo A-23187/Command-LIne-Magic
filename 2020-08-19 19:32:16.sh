098a897b1d24d4af3170b1179173a8f4f73461fd
A23187 <a23187.1979421048@gmail.com>
2020-08-20 01:08:54 +0000

    Add '2020-08-19 19:32:16.sh'

---
+++ 2020-08-19 19:32:16.sh
@@ -0,0 +1,4 @@
+# Because sometimes one -v option isn't enough to figure out why you can't connect, you can increase the debug level in the ssh client to it's max with three v options. (Updated on 2020-08-19 19:32:16)
+
+ssh -vvv remotehost
+
