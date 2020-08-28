7e21fcd4153bd9fb61995e3917e1d49f290eab08
A23187 <a23187.1979421048@gmail.com>
2020-08-28 01:12:10 +0000

    Add '2020-08-27 19:33:13.sh'

---
+++ 2020-08-27 19:33:13.sh
@@ -0,0 +1,4 @@
+# Convert a SVG vector graphics file into a PNG raster graphics file. This uses brace expansion to generate the names to pass to ImageMagick's convert command. (Updated on 2020-08-27 19:33:13)
+
+convert company-logo.{svg,png}
+
