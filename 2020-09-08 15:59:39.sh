4a481472e0a02793d7b21ce8540263f641203995
A23187 <a23187.1979421048@gmail.com>
2020-09-09 01:14:09 +0000

    Add '2020-09-08 15:59:39.sh'

---
+++ 2020-09-08 15:59:39.sh
@@ -0,0 +1,4 @@
+# Pretend that you're on the Enterprise for #StarTrekDay. Uses play from the sox package. (Updated on 2020-09-08 15:59:39)
+
+play -n -c1 synth pinknoise band -n 100 10 band -n 60 20 gain +20 fade h 1 86400 1
+
