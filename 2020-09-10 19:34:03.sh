d407fdbc22a3eb489ed262e415b595a63c52ca91
A23187 <a23187.1979421048@gmail.com>
2020-09-11 01:12:18 +0000

    Add '2020-09-10 19:34:03.sh'

---
+++ 2020-09-10 19:34:03.sh
@@ -0,0 +1,4 @@
+# The espeak version. (Updated on 2020-09-10 19:34:03)
+
+for voice in {1..15}; do espeak -v en "We are the Borg. Lower your shields and surrender your ships... Your biological and technological distinctiveness will be added to our own. Resistance is futile." & sleep 0.005; done
+
