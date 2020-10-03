30d923fcf58707cd6771b9155c80bcfa6ae24906
A23187 <a23187.1979421048@gmail.com>
2020-10-03 01:20:52 +0000

    Add '2020-10-02 19:43:48.sh'

---
+++ 2020-10-02 19:43:48.sh
@@ -0,0 +1,4 @@
+# A command line based file manager that is quick to use, quick to learn and does many of the file manager things you'd probably want to do. Yes, it really is 3 letter n's. (Updated on 2020-10-02 19:43:48)
+
+nnn
+
