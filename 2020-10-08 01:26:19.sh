1902d5d32f4efa792b5a10bd53733ca3be0a7846
A23187 <a23187.1979421048@gmail.com>
2020-10-08 13:10:37 +0000

    Add '2020-10-08 01:26:19.sh'

---
+++ 2020-10-08 01:26:19.sh
@@ -0,0 +1,4 @@
+# Check the length of a word in terms of it's character count. Although this word doesn't contain unicode characters, using the -m option would give you the expected answer if it did. (Updated on 2020-10-08 01:26:19)
+
+echo -n pentacosiomedimni | wc -m
+
