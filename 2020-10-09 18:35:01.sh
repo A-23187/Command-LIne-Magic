ef8ce323f34accf55f27ec410dfa27893b960ef6
A23187 <a23187.1979421048@gmail.com>
2020-10-10 01:25:43 +0000

    Add '2020-10-09 18:35:01.sh'

---
+++ 2020-10-09 18:35:01.sh
@@ -0,0 +1,4 @@
+# Make an impact with ImageMagick. (Updated on 2020-10-09 18:35:01)
+
+convert -size 1000x600 -define gradient:radii=1000,500 radial-gradient:#884b88-#010101 -font Impact -pointsize 72 -fill white -gravity center -interline-spacing 50 -annotate 0,0 "Now with more arguments!" now-with-more-arguments.png 
+
