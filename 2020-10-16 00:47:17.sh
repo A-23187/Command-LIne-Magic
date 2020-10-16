bd6a3a9954f3b6784e7508a1a73afcca9928f9d3
A23187 <a23187.1979421048@gmail.com>
2020-10-16 01:31:02 +0000

    Add '2020-10-16 00:47:17.sh'

---
+++ 2020-10-16 00:47:17.sh
@@ -0,0 +1,4 @@
+# Print out the character width and height of your terminal so that you generally know what will fit. This is a bashism, but works in some other shells too. (Updated on 2020-10-16 00:47:17)
+
+echo ${COLUMNS}x${LINES}
+
