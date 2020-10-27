74422c726217937d8be405dda9a95cfad2a0b5ec
A23187 <a23187.1979421048@gmail.com>
2020-10-27 01:39:43 +0000

    Add '2020-10-26 15:47:10.sh'

---
+++ 2020-10-26 15:47:10.sh
@@ -0,0 +1,4 @@
+# Use tsocks to force programs to use SOCKS5. Even if they already have the ability, you may get more flexibility using it. You configure it in $HOME/.tsocks.conf or /etc/tsocks.conf and then just precede whatever program you want to proxy with 'tsocks '. (Updated on 2020-10-26 15:47:10)
+
+tsocks thunderbird
+
