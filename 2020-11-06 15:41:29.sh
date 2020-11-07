42bcbe14592c8cc62f543bf5319cb9be34a79947
A23187 <a23187.1979421048@gmail.com>
2020-11-07 01:16:52 +0000

    Add '2020-11-06 15:41:29.sh'

---
+++ 2020-11-06 15:41:29.sh
@@ -0,0 +1,4 @@
+# Check connectivity/latency to a network host. Kind of a real time ping/traceroute hybrid. Try the different display modes using the 'd' key. [Video:mtr-demo-2020-11-06_10.37.29.mp4] (Updated on 2020-11-06 15:41:29)
+
+mtr 74.6.143.25
+
