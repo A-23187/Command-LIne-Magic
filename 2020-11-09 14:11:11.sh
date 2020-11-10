1f60596074a5c15bfdda5c45357a3e57b73b74ae
A23187 <a23187.1979421048@gmail.com>
2020-11-10 01:22:59 +0000

    Add '2020-11-09 14:11:11.sh'

---
+++ 2020-11-09 14:11:11.sh
@@ -0,0 +1,4 @@
+# Ever want to print out what the value is going to be in a loop or something before you actually run it, but think typing is wasteful? This is one approach that saves a whopping 4 characters. Other ideas? (Updated on 2020-11-09 14:11:11)
+
+C="sleep $RANDOM" ; echo "$C" ; $C
+
