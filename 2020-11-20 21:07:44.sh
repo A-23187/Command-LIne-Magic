0cf30c44612fa6641bc45fe5bb4ba7262b525a4b
A23187 <a23187.1979421048@gmail.com>
2020-11-21 01:17:29 +0000

    Add '2020-11-20 21:07:44.sh'

---
+++ 2020-11-20 21:07:44.sh
@@ -0,0 +1,4 @@
+# Use the scaletempo audio filter option with mplayer so that people don't sound like chipmunks when you increase the playback speed. (Updated on 2020-11-20 21:07:44)
+
+mplayer -af scaletempo video.mp4
+
