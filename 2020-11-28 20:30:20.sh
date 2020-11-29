105780b944570a9adccbe4fc6d4235685e4b84a3
A23187 <a23187.1979421048@gmail.com>
2020-11-29 01:26:26 +0000

    Add '2020-11-28 20:30:20.sh'

---
+++ 2020-11-28 20:30:20.sh
@@ -0,0 +1,4 @@
+# You can combine two or more images together using Imagemagick's montage command, sometimes in humorous ways. (Updated on 2020-11-28 20:30:20)
+
+montage ralph-bids.png ebay-ps5-7600dollars-screenshot.png -geometry 1092x600+500+0 -tile 1x2  ralph-bids-on-ps5.png 
+
