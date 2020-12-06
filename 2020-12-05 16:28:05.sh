f20bfe377a7ef6fc201d88425e2f625b34450b62
A23187 <a23187.1979421048@gmail.com>
2020-12-06 01:28:04 +0000

    Add '2020-12-05 16:28:05.sh'

---
+++ 2020-12-05 16:28:05.sh
@@ -0,0 +1,4 @@
+# Alexa loses her "breath" trying to read out this number. [Video:99factorial-alexa.mp4] (Updated on 2020-12-05 16:28:05)
+
+echo "Alexa, what is 99 factorial?" | { espeak || say; }
+
