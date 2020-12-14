5c7ba6279646ed4c4a5042a7bc55cfad1d384d28
A23187 <a23187.1979421048@gmail.com>
2020-12-14 13:06:34 +0000

    Add '2020-12-14 12:45:32.sh'

---
+++ 2020-12-14 12:45:32.sh
@@ -0,0 +1,4 @@
+# Running xargs alone allows you to paste text into it, then press <Ctrl-D> to end the input and it will unwrap all the lines into one line. I just used this to put a bunch of email addresses on one line. [Video:xargs-paste-2020-12-14_07.17.33.mp4] (Updated on 2020-12-14 12:45:32)
+
+xargs
+
