f7f917d2b080cbb9594b99086508d337548598de
A23187 <a23187.1979421048@gmail.com>
2020-12-29 01:58:16 +0000

    Add '2020-12-28 17:22:37.sh'

---
+++ 2020-12-28 17:22:37.sh
@@ -0,0 +1,4 @@
+# List the file systems so that their space used/available is shown in gigabytes. This is more consistent than using just -h to show human readable. Good for if you have several and want to compare. (Updated on 2020-12-28 17:22:37)
+
+df -BG
+
