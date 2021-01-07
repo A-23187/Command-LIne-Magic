8850a6139ad4b1961f563e4eef2153f1b55fbcbe
A23187 <a23187.1979421048@gmail.com>
2021-01-07 02:14:34 +0000

    Add '2021-01-06 16:35:18.sh'

---
+++ 2021-01-06 16:35:18.sh
@@ -0,0 +1,4 @@
+# Replace all spaces in .html files in the current directory with underscores. This uses Larry Wall's rename command, not the MySQL one. (Updated on 2021-01-06 16:35:18)
+
+rename 's/ /_/g' *.html
+
