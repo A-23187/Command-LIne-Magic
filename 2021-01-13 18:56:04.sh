54e2ea9802a0e14b2be90d6ad65e366952dac0cb
A23187 <a23187.1979421048@gmail.com>
2021-01-14 02:37:58 +0000

    Add '2021-01-13 18:56:04.sh'

---
+++ 2021-01-13 18:56:04.sh
@@ -0,0 +1,4 @@
+# Read some random documentation to find some interesting commands. q to go to the next one. Ctrl-C to end it. (Updated on 2021-01-13 18:56:04)
+
+cd /usr/bin ; ls -1 | shuf | xargs man
+
