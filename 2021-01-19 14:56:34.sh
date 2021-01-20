1c3af16c4e699753c633171e57fa6c7a01980fe8
A23187 <a23187.1979421048@gmail.com>
2021-01-20 02:41:07 +0000

    Add '2021-01-19 14:56:34.sh'

---
+++ 2021-01-19 14:56:34.sh
@@ -0,0 +1,4 @@
+# swaks is a well featured SMTP testing tool for mail administrators. It allows you to override DNS MX records so you can test a new server before switching to it. (Updated on 2021-01-19 14:56:34)
+
+swaks --from me@example.com --to you@example.com --server 1.2.3.4
+
