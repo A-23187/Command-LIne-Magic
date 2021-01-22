78b7a727322f196a92fdfcc7e0faeefcf41ee2ff
A23187 <a23187.1979421048@gmail.com>
2021-01-22 02:38:23 +0000

    Add '2021-01-21 18:40:15.sh'

---
+++ 2021-01-21 18:40:15.sh
@@ -0,0 +1,4 @@
+# Automatically installs your SSH public key to the remote host (this is included in the openssh package). Need an alternate SSH port, just use '-p PORTNUM user@remotehost' (Updated on 2021-01-21 18:40:15)
+
+ssh-copy-id 'user@remotehost'
+
