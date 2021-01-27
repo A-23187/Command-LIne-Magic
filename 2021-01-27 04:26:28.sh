f8a173fc9cfc849ab2725bb39ed1330a25f3fc71
A23187 <a23187.1979421048@gmail.com>
2021-01-27 13:15:40 +0000

    Add '2021-01-27 04:26:28.sh'

---
+++ 2021-01-27 04:26:28.sh
@@ -0,0 +1,4 @@
+# We don't use open source software to be constrained. This sets your The X Window System&trade; key repeat delay to 220ms and repeats to 60 characters per second. A lot of the GUIs limit you to like 250ms and 30cps. (Updated on 2021-01-27 04:26:28)
+
+xset r rate 220 60
+
