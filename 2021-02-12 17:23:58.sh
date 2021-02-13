371b279cfa0e152dbc98797ff4d25c95af233fc6
A23187 <a23187.1979421048@gmail.com>
2021-02-13 01:27:52 +0000

    Add '2021-02-12 17:23:58.sh'

---
+++ 2021-02-12 17:23:58.sh
@@ -0,0 +1,4 @@
+# less can be used with some PDF documents to just view just the text part if it has it. This can make searching faster than in most PDF viewers and you can use regular expressions. *evil grin*. It uses lesspipe and usually pdf2txt to pull this off. (Updated on 2021-02-12 17:23:58)
+
+less roman-empire.pdf
+
