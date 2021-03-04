04c14a641d626a63c36335f9b2d3968c6e29d22c
A23187 <a23187.1979421048@gmail.com>
2021-03-04 01:33:38 +0000

    Add '2021-03-03 12:28:09.sh'

---
+++ 2021-03-03 12:28:09.sh
@@ -0,0 +1,4 @@
+# pdfgrep has most of the features of normal grep and does things that probably no PDF reader is going to do. Such as regular expressions, multi-line matching, and multiple expressions. #History (Updated on 2021-03-03 12:28:09)
+
+pdfgrep -C3 -ni -e Egypt.*Hadrian -e Hadrian.*Egypt A-Companion-to-the-Roman-Army.pdf
+
