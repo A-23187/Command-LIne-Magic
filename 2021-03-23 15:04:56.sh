579fc5611f2fba0e565d83b221fead2ac7d3bc6b
A23187 <a23187.1979421048@gmail.com>
2021-03-24 01:41:36 +0000

    Add '2021-03-23 15:04:56.sh'

---
+++ 2021-03-23 15:04:56.sh
@@ -0,0 +1,4 @@
+# Often we have these extra supplemental package management systems in play on hosts that you have to contend with. Sometimes you end up with conflicts from this confusion and overlap. What others are out there? (Updated on 2021-03-23 15:04:56)
+
+flatpak list ; snap list ; npm ls ; pip3 list
+
