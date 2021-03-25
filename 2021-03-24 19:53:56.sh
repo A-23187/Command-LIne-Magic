6e3497946519a3e81fbf45970e7c8b1aad75e44b
A23187 <a23187.1979421048@gmail.com>
2021-03-25 01:07:02 +0000

    Add '2021-03-24 19:53:56.sh'

---
+++ 2021-03-24 19:53:56.sh
@@ -0,0 +1,4 @@
+# The || logical OR operator will run the second command only if the first command fails. For instance, this will only run 'man ascii' if you the ascii command fails or is not found. (Updated on 2021-03-24 19:53:56)
+
+ascii || man ascii
+
