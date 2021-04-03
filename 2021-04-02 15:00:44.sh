227b37581edb0ceb95f88de00989668039151b3c
A23187 <a23187.1979421048@gmail.com>
2021-04-03 01:24:06 +0000

    Add '2021-04-02 15:00:44.sh'

---
+++ 2021-04-02 15:00:44.sh
@@ -0,0 +1,4 @@
+# No description found (Updated on 2021-04-02 15:00:44)
+
+bash: type functionnamezsh: whence -f functionname# Sometimes it's helpful to see how a function was defined. It's a bit different in each shell. Maybe other shell users (ksh, tcsh, fish, etc.) can explain how this is done on their platforms.
+
