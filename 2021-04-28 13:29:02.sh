22cabc9656f94026455a89b5b84f965e9361131e
A23187 <a23187.1979421048@gmail.com>
2021-04-29 01:24:35 +0000

    Add '2021-04-28 13:29:02.sh'

---
+++ 2021-04-28 13:29:02.sh
@@ -0,0 +1,4 @@
+# Want to see how far along that cp is? Just run progress and it will show you the progress of a running GNU coreutils program on Linux. Check man page for more possibilities. (Updated on 2021-04-28 13:29:02)
+
+progress
+
