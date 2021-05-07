163eb61a52c74cd0bec63ad028ea3f29f0257465
A23187 <a23187.1979421048@gmail.com>
2021-05-07 01:55:48 +0000

    Add '2021-05-06 16:55:36.sh'

---
+++ 2021-05-06 16:55:36.sh
@@ -0,0 +1,4 @@
+# Search for "ereg" in only files ending in .phtml within the current directory tree. (It's #RestoreAnOldWebsiteDay) (Updated on 2021-05-06 16:55:36)
+
+grep -r ereg . --include='*.phtml'
+
