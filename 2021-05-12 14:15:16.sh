e7a977f994120c32a4b19fee34954d7386516f07
A23187 <a23187.1979421048@gmail.com>
2021-05-13 02:07:38 +0000

    Add '2021-05-12 14:15:16.sh'

---
+++ 2021-05-12 14:15:16.sh
@@ -0,0 +1,4 @@
+# You can use the tesseract OCR software to attempt to read text from an image and print it on stdout.(example text is from Carl Sagan's book Demon-Haunted World, p331) (Updated on 2021-05-12 14:15:16)
+
+tesseract book-text1.jpg -
+
