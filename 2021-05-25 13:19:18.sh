36ffe3bd82d4dcced9c11c37eff91d5237a7483c
A23187 <a23187.1979421048@gmail.com>
2021-05-25 13:31:01 +0000

    Add '2021-05-25 13:19:18.sh'

---
+++ 2021-05-25 13:19:18.sh
@@ -0,0 +1,4 @@
+# Set the timestamp on the file shakedown.mp4 to be May 4th, 1979 at 9:15pm. I did this to test if a cloud storage system would preserve the modified time. It did. (Updated on 2021-05-25 13:19:18)
+
+touch -t 197905042115 shakedown.mp4
+
