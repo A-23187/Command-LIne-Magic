152486679203cd3c57daf64202d7f29616d20b99
A23187 <a23187.1979421048@gmail.com>
2021-05-28 03:23:11 +0000

    Add '2021-05-27 14:33:16.sh'

---
+++ 2021-05-27 14:33:16.sh
@@ -0,0 +1,4 @@
+# Show which version of Mac OS X you are on. Unfortunately this won't show you version code names like Catalina or Snarky Snow Lemur. Anyone know a command for that? #osx #macosx (Updated on 2021-05-27 14:33:16)
+
+sw_vers -productVersion
+
