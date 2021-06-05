5955287f810ca9ad179e7513bd81b45be5d8d88d
A23187 <a23187.1979421048@gmail.com>
2021-06-05 02:58:08 +0000

    Add '2021-06-04 14:48:47.sh'

---
+++ 2021-06-04 14:48:47.sh
@@ -0,0 +1,4 @@
+# Long list (-l) the files in the current directory, ordering by size (-S) in ascending order (-r = reverse) and convert sizes into a human readable form (-h) like MB, GB, etc. (Updated on 2021-06-04 14:48:47)
+
+ls -lSrh
+
