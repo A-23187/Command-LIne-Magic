75f1ea65046336859d3c2c2f78678b3c4b9a73fc
A23187 <a23187.1979421048@gmail.com>
2021-06-06 02:51:57 +0000

    Add '2021-06-05 15:23:53.sh'

---
+++ 2021-06-05 15:23:53.sh
@@ -0,0 +1,4 @@
+# It's a GNU ls thing. List the directory and group the directories first. The long option is --group-directories-first, which is descriptive, but a handful. GNU programs allow you to shorten long options to the shortest unambiguous form. Currently, this is the shortest. (Updated on 2021-06-05 15:23:53)
+
+ls --g
+
