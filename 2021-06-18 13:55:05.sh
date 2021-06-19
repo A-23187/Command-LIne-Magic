c9bb717814e440abd27a2cbf2561b6c5cc617ccf
A23187 <a23187.1979421048@gmail.com>
2021-06-19 01:57:28 +0000

    Add '2021-06-18 13:55:05.sh'

---
+++ 2021-06-18 13:55:05.sh
@@ -0,0 +1,4 @@
+# Convert a docx file into a markdown file. Great for if you want to do your formating WYSIWIG first, then put it on Github or something. (Updated on 2021-06-18 13:55:05)
+
+pandoc -o Readme.md Readme.docx
+
