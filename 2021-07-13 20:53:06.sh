13fdd967353b5b4864001e4aa1adacf83951427c
A23187 <a23187.1979421048@gmail.com>
2021-07-14 01:45:42 +0000

    Add '2021-07-13 20:53:06.sh'

---
+++ 2021-07-13 20:53:06.sh
@@ -0,0 +1,4 @@
+# Try to determine the file type of an unknown file called 'mysteryfile' using the file command. If it just says 'data', then it can't figure it out. (Updated on 2021-07-13 20:53:06)
+
+file mysteryfile
+
