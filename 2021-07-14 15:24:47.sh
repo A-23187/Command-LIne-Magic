e7218649cd15e7226404ae4cf2f14b8d8fc8c8d9
A23187 <a23187.1979421048@gmail.com>
2021-07-15 01:51:09 +0000

    Add '2021-07-14 15:24:47.sh'

---
+++ 2021-07-14 15:24:47.sh
@@ -0,0 +1,4 @@
+# Automatic Hellevator generator for #Terraria using the command line. Thanks for the music @HimekoKatagiri https://archive.org/details/lf149m (Updated on 2021-07-14 15:24:47)
+
+sleep 3 ; for i in {1..1000} ; do xdotool mousemove_relative 10 0 ; sleep 0.15 ; xdotool mousemove_relative -- -10 0 ; sleep 0.15 ; done
+
