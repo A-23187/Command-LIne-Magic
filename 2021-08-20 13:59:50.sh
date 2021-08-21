679fbed3387b20da593c336190ccd520fec9f058
A23187 <a23187.1979421048@gmail.com>
2021-08-21 01:44:41 +0000

    Add '2021-08-20 13:59:50.sh'

---
+++ 2021-08-20 13:59:50.sh
@@ -0,0 +1,4 @@
+# Establish an SSL connection to an IMAP SSL server. Shows the SSL certificate, etc. so you can check it. (Updated on 2021-08-20 13:59:50)
+
+openssl s_client -connect imap.example.com:993
+
