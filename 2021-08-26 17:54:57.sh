5678816b0fed985c876421b9e681d5d03798ed75
A23187 <a23187.1979421048@gmail.com>
2021-08-27 01:50:23 +0000

    Add '2021-08-26 17:54:57.sh'

---
+++ 2021-08-26 17:54:57.sh
@@ -0,0 +1,4 @@
+# In Linux, if your webcam supports it, this will set the zoom on your webcam to a higher value than the default (100) so that you can hide more of your surroundings in a conference call and make your head appear bigger. (Updated on 2021-08-26 17:54:57)
+
+v4l2-ctl -c zoom_absolute=160
+
