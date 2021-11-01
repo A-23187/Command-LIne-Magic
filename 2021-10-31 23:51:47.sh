1879038d650e00c707c430f2f422c92b11403d9d
A23187 <a23187.1979421048@gmail.com>
2021-11-01 01:51:44 +0000

    Add '2021-10-31 23:51:47.sh'

---
+++ 2021-10-31 23:51:47.sh
@@ -0,0 +1,4 @@
+# No description found (Updated on 2021-10-31 23:51:47)
+
+Play the melody from film "Halloween". bit.ly/3jRRPmw(base64 -d <<<TVRoZAAAAAYAAQABAYBNVHJrAAAoMAD/UQOZlmY=;notes=("4E" "55" "56");for x in {0..31}; do echo -n 1001001020|while read -n1 i;do printf "\x00\x90\x${notes[$i]}\x7F\x08\x80\x${notes[$i]}\x7F";done; done)|timidity -
+
