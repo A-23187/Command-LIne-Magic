ba1bafec2fded80be92f2fc4ecb272756d461d41
A23187 <a23187.1979421048@gmail.com>
2021-11-03 01:48:10 +0000

    Add '2021-11-02 17:42:02.sh'

---
+++ 2021-11-02 17:42:02.sh
@@ -0,0 +1,4 @@
+# No description found (Updated on 2021-11-02 17:42:02)
+
+Use a regex to detect when an invisbile unicode character has been inserted into a word to try to thwart detection for phishing campaigns.printf "MICRO&shy;SOFT" | egrep M.?I.?C.?R.?O.?S.?O.?F.?T
+
