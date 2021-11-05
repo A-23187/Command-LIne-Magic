1e8cdf5b24d5bbbf37c159a828a9800da18c0db9
A23187 <a23187.1979421048@gmail.com>
2021-11-05 01:49:58 +0000

    Add '2021-11-04 20:33:34.sh'

---
+++ 2021-11-04 20:33:34.sh
@@ -0,0 +1,4 @@
+# No description found (Updated on 2021-11-04 20:33:34)
+
+If you're finding that the output of grep is too much for you and you just want a list of the files that have matches, then just use -l. Pair it with -r and you can locate all the files with a match under a directory tree.grep -rl myusername Documents
+
