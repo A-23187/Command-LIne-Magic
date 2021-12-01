66764a68f718229d2fc47ebf0a7205812a152a0f
A23187 <a23187.1979421048@gmail.com>
2021-12-01 01:54:56 +0000

    Add '2021-11-30 18:11:23.sh'

---
+++ 2021-11-30 18:11:23.sh
@@ -0,0 +1,4 @@
+# No description found (Updated on 2021-11-30 18:11:23)
+
+I wanted to check a website for files changed in the last 30 days when I know nothing "should" have changed.find www -mtime -30 -type f | less
+
