5de1886274ae374f088b347fa4cef0bbb10063b7
A23187 <a23187.1979421048@gmail.com>
2021-12-03 01:50:16 +0000

    Add '2021-12-02 17:49:41.sh'

---
+++ 2021-12-02 17:49:41.sh
@@ -0,0 +1,4 @@
+# No description found (Updated on 2021-12-02 17:49:41)
+
+I wanted to play a bunch of mp3 files in random order and also turn off the album display window that 'mpv' creates if it has that data.mpv --shuffle --no-audio-display *.mp3
+
