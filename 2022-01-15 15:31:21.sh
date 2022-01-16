82b2e777d931176bcddde6fe3a9e9055cc90016e
A23187 <a23187.1979421048@gmail.com>
2022-01-16 02:01:57 +0000

    Add '2022-01-15 15:31:21.sh'

---
+++ 2022-01-15 15:31:21.sh
@@ -0,0 +1,4 @@
+# No description found (Updated on 2022-01-15 15:31:21)
+
+If you need to find out when some event 15 days, 23 hours and 28 minutes from now will actually take place, you can useGNU date: date -d "now + 15 days 23 hours 28 minutes"BSD date: date -v+15d -v+23H -v+28M
+
