27bfcdb04fd2933ce229c017f01ea1a98c30e418
A23187 <a23187.1979421048@gmail.com>
2022-01-22 01:44:38 +0000

    Add '2022-01-21 14:09:02.sh'

---
+++ 2022-01-21 14:09:02.sh
@@ -0,0 +1,4 @@
+# No description found (Updated on 2022-01-21 14:09:02)
+
+Play a G major chord "on guitar" using the 'play' command from the sox package.play -n synth pl G2 pl B2 pl D3 pl G3 pl D4 pl G4 delay 0 .05 .1 .15 .2 .25 remix - fade 0 4 .1 norm -1
+
