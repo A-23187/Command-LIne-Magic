c9f47c775322405c0b85c8f532a606bd6b6ece07
A23187 <a23187.1979421048@gmail.com>
2022-01-25 01:56:13 +0000

    Add '2022-01-24 18:20:34.sh'

---
+++ 2022-01-24 18:20:34.sh
@@ -0,0 +1,4 @@
+# No description found (Updated on 2022-01-24 18:20:34)
+
+I wanted to see if a seemingly stalled transfer was still trying anything, so I ran ifconfig, then wanted to run it again an hour later for byte value comparison, but only look at the bytes lines.sleep 1h ; ifconfig eth0 | grep bytes 
+
