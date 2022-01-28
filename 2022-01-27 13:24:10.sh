8432fe4baad6799dca6864b4c6dbc1a68bf90f5c
A23187 <a23187.1979421048@gmail.com>
2022-01-28 01:50:10 +0000

    Add '2022-01-27 13:24:10.sh'

---
+++ 2022-01-27 13:24:10.sh
@@ -0,0 +1,4 @@
+# No description found (Updated on 2022-01-27 13:24:10)
+
+Mozart's Birthday!q=0.25;e=0.125;for i in G\ $q 0\ $e D\ $e G\ $q 0\ $e D\ $e G\ $e D\ $e G\ $e B\ $e D5\ $e 0\ $q C5\ $q 0\ $e A\ $e C5\ $q 0\ $e A\ $e C5\ $e A\ $e F#\ $e A\ $e D\ $e;do a=($i);n=${a[0]};l=${a[1]};play -n synth saw $n trim 0 $l chorus 0.8 8 21 0.7 4 0.5 -t;done
+
