5c8f5216c289b460f4b9f4f9345404b017e2d63a
A23187 <a23187.1979421048@gmail.com>
2022-02-03 01:46:05 +0000

    Add '2022-02-02 14:47:17.sh'

---
+++ 2022-02-02 14:47:17.sh
@@ -0,0 +1,4 @@
+# No description found (Updated on 2022-02-02 14:47:17)
+
+Ever want to search with grep and preserve the color coding in the output. Use the --color=always option and less' -R (RAW) option.grep --color=always management access_log | less -R
+
