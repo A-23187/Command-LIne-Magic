bfb3cd2bdf85a2c5f7f6f4cfbf2582fe1d780ba3
A23187 <a23187.1979421048@gmail.com>
2022-03-03 02:06:35 +0000

    Add '2022-03-02 14:32:24.sh'

---
+++ 2022-03-02 14:32:24.sh
@@ -0,0 +1,4 @@
+# No description found (Updated on 2022-03-02 14:32:24)
+
+Instead of closing the game, you can pause its processing and save CPU/GPU by sending it a STOP signal for the matching process name. Not advised on all games.pkill -STOP "Stardew Valley"pkill -CONT "Stardew Valley"
+
