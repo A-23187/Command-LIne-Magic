8d3a766faa32b64c419660b95e9d99cebc4bc9dc
A23187 <a23187.1979421048@gmail.com>
2022-03-25 02:13:20 +0000

    Add '2022-03-24 14:08:12.sh'

---
+++ 2022-03-24 14:08:12.sh
@@ -0,0 +1,4 @@
+# No description found (Updated on 2022-03-24 14:08:12)
+
+If available, show the last modified time of the document as reported by the webserver. Interestingly in this case, it is 14 years earlier than the date displayed on the page itself. *shrug*curl -sLI "http://www.internic.net/faqs/new-tlds.html"| grep Last-Modified:
+
