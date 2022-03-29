8e0f6798fba78d87637eb993bdae4eb3f0dee45f
A23187 <a23187.1979421048@gmail.com>
2022-03-29 02:26:18 +0000

    Add '2022-03-28 18:14:55.sh'

---
+++ 2022-03-28 18:14:55.sh
@@ -0,0 +1,4 @@
+# No description found (Updated on 2022-03-28 18:14:55)
+
+Convert a SVG vector graphics file at 300 dpi into a PNG raster graphics file. This uses brace expansion to generate the names to pass to ImageMagick's convert command.convert -density 300 company-logo.{svg,png}
+
