292952907ec181f08130440e01a1efa539283cd5
A23187 <a23187.1979421048@gmail.com>
2022-04-20 03:01:40 +0000

    Add '2022-04-19 13:34:25.sh'

---
+++ 2022-04-19 13:34:25.sh
@@ -0,0 +1,4 @@
+# No description found (Updated on 2022-04-19 13:34:25)
+
+Ever want to make your shell glob wildcards be case INsensitive?Well these settings will do the trick:bash: shopt -s nocaseglobzsh: unsetopt CASE_GLOB
+
