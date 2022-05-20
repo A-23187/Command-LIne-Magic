779881632b42bc4807471344bf8a1b2398692ee0
A23187 <a23187.1979421048@gmail.com>
2022-05-20 13:29:05 +0000

    Add '2022-05-20 13:21:02.sh'

---
+++ 2022-05-20 13:21:02.sh
@@ -0,0 +1,4 @@
+# No description found (Updated on 2022-05-20 13:21:02)
+
+"Daddy, my teacher said there are no words in English that end in j."Well let's find out. This command will give you a frequency count of ending letters in words of 2 or more characters.for i in {a..z} ; do printf "$i " ; look . | egrep ".$i$" | wc -l ; done | sort -k2nr
+
