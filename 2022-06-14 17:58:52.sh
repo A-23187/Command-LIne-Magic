d2e3575507907eafbfeb7ded60985a31998012d5
A23187 <a23187.1979421048@gmail.com>
2022-06-15 03:02:37 +0000

    Add '2022-06-14 17:58:52.sh'

---
+++ 2022-06-14 17:58:52.sh
@@ -0,0 +1,4 @@
+# No description found (Updated on 2022-06-14 17:58:52)
+
+When you want to only run a command for a set amount of time and it doesn't have a timeout option or you don't remember what it is, you can use the 'timeout' command. Part of GNU coreutils.timeout 10m mpv music-mix.mp3
+
