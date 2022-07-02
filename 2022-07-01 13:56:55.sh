8c184256dead043bbb316e912f7757ec327eebe7
A23187 <a23187.1979421048@gmail.com>
2022-07-02 03:05:17 +0000

    Add '2022-07-01 13:56:55.sh'

---
+++ 2022-07-01 13:56:55.sh
@@ -0,0 +1,4 @@
+# No description found (Updated on 2022-07-01 13:56:55)
+
+Sometimes you need to stop Linux console messages so that you can actually do something without the logging spam. Of course, you have to remember/understand this command. Maybe print it and tape it to the console monitor. Use at your own risk.sysctl -w kernel.printk="1 4 1 7"
+
