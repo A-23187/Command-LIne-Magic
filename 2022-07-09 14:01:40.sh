24f6af95f88c38f038d366f4e0e9f2297e1bd58d
A23187 <a23187.1979421048@gmail.com>
2022-07-10 03:05:18 +0000

    Add '2022-07-09 14:01:40.sh'

---
+++ 2022-07-09 14:01:40.sh
@@ -0,0 +1,4 @@
+# No description found (Updated on 2022-07-09 14:01:40)
+
+Want to move a file into the directory you were just in? Just use the environment variable storing the previous directory.mv file "$OLDPWD/"
+
