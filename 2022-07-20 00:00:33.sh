a1b78ba5fa00bdc773469d74ffb918553f78c095
A23187 <a23187.1979421048@gmail.com>
2022-07-20 03:05:08 +0000

    Add '2022-07-20 00:00:33.sh'

---
+++ 2022-07-20 00:00:33.sh
@@ -0,0 +1,4 @@
+# No description found (Updated on 2022-07-20 00:00:33)
+
+Wanted to see the biggest zip files in my Downloads directory in order to remove unneeded ones. So I listed them (ls -l) by human readable size (-S and -h) so the largest appeared last (-r).ls -lSrh ~/Downloads/*.zip
+
