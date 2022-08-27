79059867dc781ca3186725d153045166bb5c4380
A23187 <a23187.1979421048@gmail.com>
2022-08-27 03:22:52 +0000

    Add '2022-08-26 17:43:07.sh'

---
+++ 2022-08-26 17:43:07.sh
@@ -0,0 +1,4 @@
+# No description found (Updated on 2022-08-26 17:43:07)
+
+Render Mona Lisa in the terminal using the Kitty terminal emulator.wget -o/dev/null -O monalisa.jpg https://climagic.org/images/Mona_Lisa-from-Wikipedia2.jpg && kitty +kitten icat monalisa.jpg
+
