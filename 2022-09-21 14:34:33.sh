07d0a0e41f4d491f8fc5a8fc03801c658f6b687b
A23187 <a23187.1979421048@gmail.com>
2022-09-22 03:30:45 +0000

    Add '2022-09-21 14:34:33.sh'

---
+++ 2022-09-21 14:34:33.sh
@@ -0,0 +1,4 @@
+# No description found (Updated on 2022-09-21 14:34:33)
+
+Change the prefix 'IMG_' to 'Trip_' at the beginning of each filename with a .JPG suffix in the current directory. (Note, this uses the Larry Wall version of the rename command)rename 's/^IMG_/Trip_/' *.JPG
+
