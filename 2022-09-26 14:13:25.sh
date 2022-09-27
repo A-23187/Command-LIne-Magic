03b014761a08cc6a0a8bc1e167cac376a56490cc
A23187 <a23187.1979421048@gmail.com>
2022-09-27 03:27:58 +0000

    Add '2022-09-26 14:13:25.sh'

---
+++ 2022-09-26 14:13:25.sh
@@ -0,0 +1,4 @@
+# No description found (Updated on 2022-09-26 14:13:25)
+
+Want to find the docs that explain time periods in cron? You can go directly to it by specifying the 5th chapter of the man pages and skip the crontab command man page in chapter 1.man 5 crontab
+
