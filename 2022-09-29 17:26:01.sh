0e4e6789efdd461289ccffc039908b89dcd2a6a2
A23187 <a23187.1979421048@gmail.com>
2022-09-30 03:50:47 +0000

    Add '2022-09-29 17:26:01.sh'

---
+++ 2022-09-29 17:26:01.sh
@@ -0,0 +1,4 @@
+# No description found (Updated on 2022-09-29 17:26:01)
+
+Using the -i option to mv and cp is always a good idea if you're not sure whether the files already exist or not in the destination Then at least it will warn you an give you a chance. You might even alias it.mv -i file.txt ../Documents/
+
