eddbdb93b6328ed45c11316fe48afcab615c627a
A23187 <a23187.1979421048@gmail.com>
2022-10-01 03:45:53 +0000

    Add '2022-09-30 15:13:18.sh'

---
+++ 2022-09-30 15:13:18.sh
@@ -0,0 +1,4 @@
+# No description found (Updated on 2022-09-30 15:13:18)
+
+sed allows you to use other characters to demarcate sections in a s/// command. Useful when you don't want to use '/' you can use a comma or nearly any ASCII character. Check this one out:sed statement<<<dated
+
