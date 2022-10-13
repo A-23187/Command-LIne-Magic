8855e6becb4dda038fb20c415d8f9eb57479b18a
A23187 <a23187.1979421048@gmail.com>
2022-10-13 03:31:34 +0000

    Add '2022-10-12 14:48:00.sh'

---
+++ 2022-10-12 14:48:00.sh
@@ -0,0 +1,4 @@
+# No description found (Updated on 2022-10-12 14:48:00)
+
+For renaming part of a long filename (to remove "Copy of "), try using brace expansion for the From/To renamed part.mv {Copy\ of\ ,}Some\ Long\ Filename.pdf
+
