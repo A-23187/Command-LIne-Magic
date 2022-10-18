ca0c3d42350a7dfc88d636e68f8c5271015ceb26
A23187 <a23187.1979421048@gmail.com>
2022-10-18 03:36:11 +0000

    Add '2022-10-17 15:35:14.sh'

---
+++ 2022-10-17 15:35:14.sh
@@ -0,0 +1,4 @@
+# No description found (Updated on 2022-10-17 15:35:14)
+
+Show your basic terminal text colors for the purpose of changing them in your preferences.printf "\e[%dm%d dark\e[0m \e[%d;1m%d bold\e[0m\n" {30..37}{,,,}
+
