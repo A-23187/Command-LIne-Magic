e286647502072d9b4940484159978ec8bd6dde86
A23187 <a23187.1979421048@gmail.com>
2022-11-17 02:41:41 +0000

    Add '2022-11-16 15:31:13.sh'

---
+++ 2022-11-16 15:31:13.sh
@@ -0,0 +1,4 @@
+# No description found (Updated on 2022-11-16 15:31:13)
+
+Find those big files!ncdu is TUI program to generate and view usage statistics for disk space from the current directory and below. Use -x to avoid crossing filesystem boundaries and disable file modification with -r. ionice will give ncdu less I/O priority.ionice ncdu -x -r
+
