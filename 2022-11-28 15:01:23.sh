e3e914a9b1784bb31269347747b9bdfcf5cac771
A23187 <a23187.1979421048@gmail.com>
2022-11-29 02:20:58 +0000

    Add '2022-11-28 15:01:23.sh'

---
+++ 2022-11-28 15:01:23.sh
@@ -0,0 +1,4 @@
+# No description found (Updated on 2022-11-28 15:01:23)
+
+Stump your friends. How many months had only 19 days? Answer: 1 (When the west switched to Gregorian.)cal 9 1752(Actual month depends on your locale and can be found with ncal -p)
+
