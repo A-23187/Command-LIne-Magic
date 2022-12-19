51b799906fb8bc2a3fec9a8746c6c1e49276c369
A23187 <a23187.1979421048@gmail.com>
2022-12-19 02:01:57 +0000

    Add '2022-12-18 22:48:36.sh'

---
+++ 2022-12-18 22:48:36.sh
@@ -0,0 +1,4 @@
+# No description found (Updated on 2022-12-18 22:48:36)
+
+Renaming a file is easy and using the -i option will ensure you're prompted before overwriting something.mv -i twitter-queueposter.py mastodon-queueposter.py
+
