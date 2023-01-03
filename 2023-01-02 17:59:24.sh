1669d6d94e454e926797373e0600b3d84a2e3ebf
A23187 <a23187.1979421048@gmail.com>
2023-01-03 02:05:22 +0000

    Add '2023-01-02 17:59:24.sh'

---
+++ 2023-01-02 17:59:24.sh
@@ -0,0 +1,4 @@
+# No description found (Updated on 2023-01-02 17:59:24)
+
+Show what processes are listening on or connecting to (-i) TCP port 443, but don't do the reverse DNS search on the hostnames and port numbers(-nP).sudo lsof -nP -i TCP:443Cc: @DerekRiemer
+
