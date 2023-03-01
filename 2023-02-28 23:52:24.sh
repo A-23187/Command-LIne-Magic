57e1598e5eba37aa1e184c0d38d3830dff7959a7
A23187 <a23187.1979421048@gmail.com>
2023-03-01 02:27:16 +0000

    Add '2023-02-28 23:52:24.sh'

---
+++ 2023-02-28 23:52:24.sh
@@ -0,0 +1,4 @@
+# No description found (Updated on 2023-02-28 23:52:24)
+
+I wanted to random sort rows of a spreadsheet. So I ran the below command to get random numbers from 1 to 40 (rows in my spreadsheet), then pasted that into a new column and sorted by that column, then removed the column.printf "%d\n" {1..40} | shuf
+
