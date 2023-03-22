94d95cbfb4f748cb5a9eaeb4134e1989c55c0a1b
A23187 <a23187.1979421048@gmail.com>
2023-03-22 02:01:00 +0000

    Add '2023-03-21 16:29:25.sh'

---
+++ 2023-03-21 16:29:25.sh
@@ -0,0 +1,4 @@
+# No description found (Updated on 2023-03-21 16:29:25)
+
+If you want to use less, but don't want to save any of your sensitive searches in the ~/.lesshst file this one time, just override the hist file variable.LESSHISTFILE=/dev/null less sensitive.dat
+
