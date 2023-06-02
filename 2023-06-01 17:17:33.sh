e9d7976b2555407844dd69f85cfbf1268048ef4e
A23187 <a23187.1979421048@gmail.com>
2023-06-02 02:17:43 +0000

    Add '2023-06-01 17:17:33.sh'

---
+++ 2023-06-01 17:17:33.sh
@@ -0,0 +1,4 @@
+# No description found (Updated on 2023-06-01 17:17:33)
+
+I think this command (not mine) is just so elegant. It's for generating both the private key and the public key for a Wireguard tunnel in one go.wg genkey | tee privatekey | wg pubkey > publickey
+
